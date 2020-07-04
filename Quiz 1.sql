5cad5260262c01f69f2badf499f09dc4f28e55ee
ShantanilBagchi <38660939+ShantanilBagchi@users.noreply.github.com>
2020-07-04 13:42:12 -0400

    Create Quiz 1.sql

---
+++ Quiz 1.sql
@@ -0,0 +1,26 @@
+--Q1) Retrieve all the records from the Employees table.
+
+SELECT * FROM Employees
+
+------------------------------------------------------------------------------------
+
+--Q2) Retrieve the FirstName, LastName, Birthdate, Address, City, and State from
+-- the Employees table.
+
+SELECT
+      FirstName,
+      LastName,
+      Birthdate,
+      Address,
+      City,
+      State
+FROM Employees
+WHERE BirthDate LIKE '1965%';
+
+------------------------------------------------------------------------------------
+
+--Q3) Retrieve all the columns from the Tracks table, but only return 20 rows.
+
+SELECT *
+FROM Tracks
+LIMIT 20;
