83121142493536ebd5b9eb3e6705dfa85a8b4d5b
ShantanilBagchi <38660939+ShantanilBagchi@users.noreply.github.com>
2020-07-04 13:43:20 -0400

    Update Data Scientist Role Play on YELP Dataset Analysis.sql

--- Data Scientist Role Play on YELP Dataset Analysis.sql
+++ Data Scientist Role Play on YELP Dataset Analysis.sql
@@ -1,29 +1,29 @@
-﻿Data Scientist Role Play: Profiling and Analyzing the Yelp Dataset Coursera Worksheet
+﻿-- Data Scientist Role Play: Profiling and Analyzing the Yelp Dataset Coursera Worksheet
 
-This is a 2-part assignment. In the first part, you are asked a series of questions that will help you profile and understand the data just like a data scientist would. For this first part of the assignment, you will be assessed both on the correctness of your findings, as well as the code you used to arrive at your answer. You will be graded on how easy your code is to read, so remember to use proper formatting and comments where necessary.
+-- This is a 2-part assignment. In the first part, you are asked a series of questions that will help you profile and understand the data just like a data scientist would. For this first part of the assignment, you will be assessed both on the correctness of your findings, as well as the code you used to arrive at your answer. You will be graded on how easy your code is to read, so remember to use proper formatting and comments where necessary.
 
-In the second part of the assignment, you are asked to come up with your own inferences and analysis of the data for a particular research question you want to answer. You will be required to prepare the dataset for the analysis you choose to do. As with the first part, you will be graded, in part, on how easy your code is to read, so use proper formatting and comments to illustrate and communicate your intent as required.
+-- In the second part of the assignment, you are asked to come up with your own inferences and analysis of the data for a particular research question you want to answer. You will be required to prepare the dataset for the analysis you choose to do. As with the first part, you will be graded, in part, on how easy your code is to read, so use proper formatting and comments to illustrate and communicate your intent as required.
 
-For both parts of this assignment, use this "worksheet." It provides all the questions you are being asked, and your job will be to transfer your answers and SQL coding where indicated into this worksheet so that your peers can review your work. You should be able to use any Text Editor (Windows Notepad, Apple TextEdit, Notepad ++, Sublime Text, etc.) to copy and paste your answers. If you are going to use Word or some other page layout application, just be careful to make sure your answers and code are lined appropriately.
-In this case, you may want to save as a PDF to ensure your formatting remains intact for you reviewer.
+-- For both parts of this assignment, use this "worksheet." It provides all the questions you are being asked, and your job will be to transfer your answers and SQL coding where indicated into this worksheet so that your peers can review your work. You should be able to use any Text Editor (Windows Notepad, Apple TextEdit, Notepad ++, Sublime Text, etc.) to copy and paste your answers. If you are going to use Word or some other page layout application, just be careful to make sure your answers and code are lined appropriately.
+-- In this case, you may want to save as a PDF to ensure your formatting remains intact for you reviewer.
 
 
 
-Part 1: Yelp Dataset Profiling and Understanding
+-- Part 1: Yelp Dataset Profiling and Understanding
 
-1. Profile the data by finding the total number of records for each of the tables below:
+-- 1. Profile the data by finding the total number of records for each of the tables below:
 	
-i. Attribute table = 10000 rows
-ii. Business table = 10000 rows
-iii. Category table = 10000 rows
-iv. Checkin table = 10000 rows
-v. elite_years table = 10000rows
-vi. friend table = 10000rows
-vii. hours table = 10000rows
-viii. photo table = 10000rows
-ix. review table = 10000 rows
-x. tip table = 10000 rows
-xi. user table = 10000 rows
+-- i. Attribute table = 10000 rows
+-- ii. Business table = 10000 rows
+-- iii. Category table = 10000 rows
+-- iv. Checkin table = 10000 rows
+-- v. elite_years table = 10000rows
+-- vi. friend table = 10000rows
+-- vii. hours table = 10000rows
+-- viii. photo table = 10000rows
+-- ix. review table = 10000 rows
+-- x. tip table = 10000 rows
+-- xi. user table = 10000 rows
 	
 
 
